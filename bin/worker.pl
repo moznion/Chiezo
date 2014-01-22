7652a9065f432bb6cd467f98315b9530c4134734
moznion <moznion@gmail.com>
2014-01-22 23:13:16 +0900

    Initial commit

---
+++ bin/worker.pl
@@ -0,0 +1,61 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+use AnyEvent;
+use FindBin;
+use Log::Minimal;
+use Unruly;
+use URL::Encode qw/url_encode_utf8/;
+use Web::Query;
+
+use constant {
+    WIKIPEDIA_BASE_URL => 'http://ja.wikipedia.org/wiki/',
+    CONTENT_NODE       => 'div#mw-content-text p b',
+};
+
+my $conf = do "$FindBin::Bin/../config.pl";
+my $YANCHA_URL = $conf->{YANCHA_URL};
+
+my $ur = Unruly->new(
+    url  => $YANCHA_URL,
+    tags => {PUBLIC => 1}
+);
+
+$ur->login('chiezo');
+
+my $cv = AnyEvent->condvar;
+
+$ur->run(sub {
+    my ($client, $socket) = @_;
+    $socket->on('user message' => sub {
+        my ($socket_info, $message) = @_;
+
+        my @tags = @{$message->{tags}};
+        my $message_text = $message->{text};
+        if ($message->{text} =~ /^w\s(.+)\s#/) {
+            my $word = $1;
+            my $url  = WIKIPEDIA_BASE_URL . url_encode_utf8($word);
+
+            my $q = Web::Query->new_from_url($url);
+            unless ($q) {
+                critf("Cannot get a resource from %s: %s", $url);
+                $ur->post("そんな用語を説明してるページないです: $word", @tags);
+                return;
+            }
+
+            my $text = $q->find(CONTENT_NODE)->first->parent->first->text;
+            unless ($text) {
+                critf("Cannot get the content text: %s", CONTENT_NODE);
+                $ur->post("多分WikipediaのDOMの構造変わった", @tags);
+                return;
+            }
+
+            my $post_text = sprintf("%s %s", substr($text, 0, 100), $url);
+            $ur->post($post_text, @tags);
+        }
+    });
+});
+
+$cv->wait;
---
+++ cpanfile
@@ -0,0 +1,8 @@
+#mirror 'http://www.cpan.org';
+#mirror 'http://pan.hachiojipm.org';
+requires 'AnyEvent';
+requires 'FindBin';
+requires 'Log::Minimal';
+requires 'Unruly';
+requires 'URL::Encode';
+requires 'Web::Query';
